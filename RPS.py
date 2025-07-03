e7c0f51bc2652363d93bee670cb12fcdb61c597a
Inf3st4toR 1 <xcharaix@gmail.com>
2025-07-03 04:34:09 +0000

    Middle save in progress

--- RPS.py
+++ RPS.py
@@ -1,10 +1,41 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import tensorflow as tf
+from tendorflow import keras
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+my_model = keras.Sequential([
+    keras.layers.Dense(16, activation='relu', input_shape=(3,)),
+    keras.layers.Dense(3, activation='softmax')
+])
+my_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def model(data0, data1):
+    X = [data[i]]
+    y = [data[i+1] for i in range(range)]
 
-    return guess
+    my_model.fit(X, y, epochs=5)
+    return prediction
+
+def beat(guess):
+    match guess:
+        case 0:
+            return "P"
+        case 1:
+            return "C"
+        case 2:
+            return "R"
+
+def player(prev_play, opponent_history=[], my_history=[]):
+    # First 3 plays
+    if len(opponent_history) < 3:
+        my_history.append(len(opponent_history))
+        if prev_play:
+            opponent_history.append(0 if prev_play == "R" else 1 if prev_play == "P" else 2)
+        return ("R" if len(opponent_history) == 0 else "P" if len(opponent_history) == 1 else "S")
+    
+    # Start cheating
+    opponent_history.append(0 if prev_play == "R" else 1 if prev_play == "P" else 2)
+    prediction = model(opponent_history, my_history)
+    my_move = beat(prediction)
+    my_history.append(0 if my_move == "R" else 1 if my_move == "P" else 2)
+    return my_move
+
+# rock 0, paper 1, cissors 2
